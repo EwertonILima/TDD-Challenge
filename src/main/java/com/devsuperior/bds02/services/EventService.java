671055ce39b9ec5a04462f09e946376d1c3014ab
EwertonILima <ewertoninaciolima@gmail.com>
2021-06-30 19:49:41 -0300

    API development trought TDD

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.bds02.services;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exception.ResourceNotFoundException;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository eventRepository;
+
+	@Autowired
+	private CityRepository cityRepository;
+
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+		try {
+			Event entity = eventRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setUrl(dto.getUrl());
+			entity.setDate(dto.getDate());
+
+			CityDTO cityDto = new CityDTO(dto.getCityId(), null);
+			City city = cityRepository.getOne(cityDto.getId());
+			entity.setCity(city);
+
+			entity = eventRepository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+}
